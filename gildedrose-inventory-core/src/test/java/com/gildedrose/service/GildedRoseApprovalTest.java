4e88d0f838d6928241a04578f271dc9f2d862cda
kanag <kanagarajaa@gmail.com>
2026-06-02 23:05:25 +0200

    refactoring-assessment: copied gilded rose files and included approval tests

---
+++ gildedrose-inventory-core/src/test/java/com/gildedrose/service/GildedRoseApprovalTest.java
@@ -0,0 +1,42 @@
+package com.gildedrose.service;
+
+import com.gildedrose.model.Item;
+import org.approvaltests.Approvals;
+import org.junit.jupiter.api.Test;
+
+public class GildedRoseApprovalTest {
+    @Test
+    public void verifyThirtyDaysOfInventoryUpdates() {
+        // Arrange
+        Item[] items = new Item[] {
+                new Item("+5 Dexterity Vest", 10, 20), //
+                new Item("Aged Brie", 2, 0), //
+                new Item("Elixir of the Mongoose", 5, 7), //
+                new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
+                new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+                new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+                new Item("Conjured Mana Cake", 3, 6) };
+
+        GildedRose gildedRose = new GildedRose(items);
+        StringBuilder textLedger = new StringBuilder();
+
+        //Act for 30 days
+        int days = 30;
+        for (int day = 0; day <= days; day++) {
+            textLedger.append("-------- day ").append(day).append(" --------\n");
+            textLedger.append("name, sellIn, quality\n");
+
+            for (Item item : items) {
+                textLedger.append(item.toString()).append("\n");
+            }
+            textLedger.append("\n");
+
+            gildedRose.updateQuality();
+        }
+
+        // Assert
+        Approvals.verify(textLedger.toString());
+    }
+}
